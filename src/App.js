ed7bfb3ee59fdc001a3075dc43ac2f352653a505
Ulker <ulkrthmzova@gmail.com>
2022-06-03 19:30:04 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class App extends Component {
+    state ={  advice:""  }
+    componentDidMount(){
+this.fetchAdvice();
+    }
+    fetchAdvice =()=>{
+        axios.get("https://api.adviceslip.com/advice")
+        .then(response=>{
+            const{advice} = response.data.slip
+            this.setState({
+                advice
+            })
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
+    handleClick =()=>{
+       return this.fetchAdvice()
+    }
+  render() {
+      const{advice} =this.state
+    return (
+      <div className ='app'>
+<div className ="card">
+<h1 className ="heading">{advice}</h1>
+<button className="button" onClick={this.handleClick}>
+    <span>
+    give me advice!
+    </span>
+    </button>
+</div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
